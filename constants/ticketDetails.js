bde7d700f21fb53ebb4155d5c482f0f4df896c0e
Kumar <snakumar@deloitte.com>
2023-02-01 09:24:23 +0530

    BE: Created the boiler plate for the backend app

---
+++ constants/ticketDetails.js
@@ -0,0 +1,186 @@
+const OPEN_TICKETS = [
+    {
+        "ticketId": "SFP-CC-E-001",
+        "machineId": "M001",
+        "machineName": "Laser Tek - CNC",
+        "line": "L001",
+        "assignedToUser": "User001",
+        "faultCode": "FC-001",
+        "faultMessage": "Unable to move gantry",
+        "startTime": "08:00:00",
+        "endTime": "12:00:00",
+        "date": "31/01/2023"
+    },
+    {
+        "ticketId": "SFP-CC-E-002",
+        "machineId": "M001",
+        "machineName": "Laser Tek - CNC",
+        "line": "L002",
+        "assignedToUser": "User001",
+        "faultCode": "FC-001",
+        "faultMessage": "Unable to move gantry",
+        "startTime": "12:00:00",
+        "endTime": "15:00:00",
+        "date": "31/01/2023"
+    },
+    {
+        "ticketId": "SFP-CC-E-003",
+        "machineId": "M001",
+        "machineName": "Laser Tek - CNC",
+        "line": "L003",
+        "assignedToUser": "User001",
+        "faultCode": "FC-001",
+        "faultMessage": "Unable to move gantry",
+        "startTime": "15:00:00",
+        "endTime": "18:00:00",
+        "date": "31/01/2023"
+    },
+    {
+        "ticketId": "SFP-CC-E-004",
+        "machineId": "M001",
+        "machineName": "Laser Tek - CNC",
+        "line": "L004",
+        "assignedToUser": "User001",
+        "faultCode": "FC-001",
+        "faultMessage": "Unable to move gantry",
+        "startTime": "18:00:00",
+        "endTime": "20:00:00",
+        "date": "31/01/2023"
+    },
+    {
+        "ticketId": "SFP-CC-E-005",
+        "machineId": "M001",
+        "machineName": "Laser Tek - CNC",
+        "line": "L005",
+        "assignedToUser": "User001",
+        "faultCode": "FC-001",
+        "faultMessage": "Unable to move gantry",
+        "startTime": "20:00:00",
+        "endTime": "23:59:00",
+        "date": "31/01/2023"
+    },
+    {
+        "ticketId": "SFP-CC-E-006",
+        "machineId": "M001",
+        "machineName": "Laser Tek - CNC",
+        "line": "L001",
+        "assignedToUser": "User002",
+        "faultCode": "FC-001",
+        "faultMessage": "Unable to move gantry",
+        "startTime": "08:00:00",
+        "endTime": "12:00:00",
+        "date": "31/01/2023"
+    },
+    {
+        "ticketId": "SFP-CC-E-007",
+        "machineId": "M001",
+        "machineName": "Laser Tek - CNC",
+        "line": "L002",
+        "assignedToUser": "User002",
+        "faultCode": "FC-001",
+        "faultMessage": "Unable to move gantry",
+        "startTime": "12:00:00",
+        "endTime": "15:00:00",
+        "date": "31/01/2023"
+    },
+    {
+        "ticketId": "SFP-CC-E-008",
+        "machineId": "M001",
+        "machineName": "Laser Tek - CNC",
+        "line": "L003",
+        "assignedToUser": "User002",
+        "faultCode": "FC-001",
+        "faultMessage": "Unable to move gantry",
+        "startTime": "15:00:00",
+        "endTime": "18:00:00",
+        "date": "31/01/2023"
+    },
+    {
+        "ticketId": "SFP-CC-E-009",
+        "machineId": "M001",
+        "machineName": "Laser Tek - CNC",
+        "line": "L004",
+        "assignedToUser": "User002",
+        "faultCode": "FC-001",
+        "faultMessage": "Unable to move gantry",
+        "startTime": "18:00:00",
+        "endTime": "20:00:00",
+        "date": "31/01/2023"
+    },
+    {
+        "ticketId": "SFP-CC-E-010",
+        "machineId": "M001",
+        "machineName": "Laser Tek - CNC",
+        "line": "L005",
+        "assignedToUser": "User002",
+        "faultCode": "FC-001",
+        "faultMessage": "Unable to move gantry",
+        "startTime": "20:00:00",
+        "endTime": "23:59:00",
+        "date": "31/01/2023"
+    },
+    {
+        "ticketId": "SFP-CC-E-011",
+        "machineId": "M001",
+        "machineName": "Laser Tek - CNC",
+        "line": "L001",
+        "assignedToUser": "User003",
+        "faultCode": "FC-001",
+        "faultMessage": "Unable to move gantry",
+        "startTime": "08:00:00",
+        "endTime": "12:00:00",
+        "date": "31/01/2023"
+    },
+    {
+        "ticketId": "SFP-CC-E-012",
+        "machineId": "M001",
+        "machineName": "Laser Tek - CNC",
+        "line": "L002",
+        "assignedToUser": "User003",
+        "faultCode": "FC-001",
+        "faultMessage": "Unable to move gantry",
+        "startTime": "12:00:00",
+        "endTime": "15:00:00",
+        "date": "31/01/2023"
+    },
+    {
+        "ticketId": "SFP-CC-E-013",
+        "machineId": "M001",
+        "machineName": "Laser Tek - CNC",
+        "line": "L003",
+        "assignedToUser": "User003",
+        "faultCode": "FC-001",
+        "faultMessage": "Unable to move gantry",
+        "startTime": "15:00:00",
+        "endTime": "18:00:00",
+        "date": "31/01/2023"
+    },
+    {
+        "ticketId": "SFP-CC-E-014",
+        "machineId": "M001",
+        "machineName": "Laser Tek - CNC",
+        "line": "L004",
+        "assignedToUser": "User003",
+        "faultCode": "FC-001",
+        "faultMessage": "Unable to move gantry",
+        "startTime": "18:00:00",
+        "endTime": "20:00:00",
+        "date": "31/01/2023"
+    },
+    {
+        "ticketId": "SFP-CC-E-015",
+        "machineId": "M001",
+        "machineName": "Laser Tek - CNC",
+        "line": "L005",
+        "assignedToUser": "User003",
+        "faultCode": "FC-001",
+        "faultMessage": "Unable to move gantry",
+        "startTime": "20:00:00",
+        "endTime": "23:59:00",
+        "date": "31/01/2023"
+    }
+];
+
+module.exports = {
+    OPEN_TICKETS,
+}
