6330d01c0d3d03b6f73a115d7f672a5361e3d926
Deepmala Kumari <deepmfnu@deloitte.com>
2023-02-22 14:59:19 +0530

    [opticrew]:adding mechanic specific changes in consolidated API

--- common/ShiftBreakTime.js
+++ common/ShiftBreakTime.js
@@ -15,17 +15,23 @@ const ShiftBreakTime = async (req, res) => {
     //to get the day number 
     daynumber = new Date().getDay()
 
-    SHIFT_BREAK_TIMINGS[daynumber].filter(ele => {
-        if (formattedTime >= ele.shiftStartTime && formattedTime <= ele.shiftEndTime) {
-            breakList.push({
-                "breakName": "break 1",
-                "breakDescription": ele.breakDescription,
-                "breakStartTime": ele.breakStartTime,
-                "breakEndTime": ele.breakEndTime
-            });
-            return  breakList ;
-        }
-    })
+    if (MECHANIC_DETAILS[userId]) {
+        mechanicShift = MECHANIC_DETAILS[userId].shift
+
+        SHIFT_BREAK_TIMINGS[daynumber].filter(ele => {
+            if (formattedTime >= ele.shiftStartTime && formattedTime <= ele.shiftEndTime) {
+                if (ele.shiftNo === mechanicShift) {
+                    breakList.push({
+                        "breakName": ele.breakName,
+                        "breakDescription": ele.breakDescription,
+                        "breakStartTime": ele.breakStartTime,
+                        "breakEndTime": ele.breakEndTime
+                    });
+                }
+                return breakList;
+            }
+        })
+    }
     return breakList;
 }
 
--- services/opticrewMechanicShiftRecordsServices.js
+++ services/opticrewMechanicShiftRecordsServices.js
@@ -2,15 +2,15 @@ const MechanicDetails = require("../common/MechanicDetails");
 const SubAreasDetails = require("../common/SubAreaDetails");
 const ShiftBreakTime = require("../common/ShiftBreakTime");
 const SHIFT_BREAK_TIMINGS = require("../constants/shift_breaktimings");
-
+const MECHANIC_DETAILS = require("../constants/mechanic_details");
 
 const getMechanicShiftRecords = async (req, res) => {
     try {
         if (!req.query.userId) {
             res.status(400).send("UserId not entered!")
         }
-
-        let mechanicLists, subAreas, breakList, shiftDetails, nextShiftTime;
+        let userId = req.query.userId;
+        let mechanicLists, subAreas, breakList, shiftDetails, mechanicShift, mechanicShiftRecords;
 
         //to get the time in 24 hr format
         let currentDateTime = new Date();
@@ -20,39 +20,41 @@ const getMechanicShiftRecords = async (req, res) => {
         //to get the day number 
         daynumber = new Date().getDay()
 
-        SHIFT_BREAK_TIMINGS[daynumber].filter(ele => {
-            if (formattedTime >= ele.shiftStartTime && formattedTime <= ele.shiftEndTime) {
-                shiftDetails = ele;
-                return shiftDetails
+        if (MECHANIC_DETAILS[userId]) {
+            mechanicShift = MECHANIC_DETAILS[userId].shift
 
+
+            SHIFT_BREAK_TIMINGS[daynumber].filter(ele => {
+                if (formattedTime >= ele.shiftStartTime && formattedTime <= ele.shiftEndTime) {
+                    console.log("shift number", mechanicShift, ele.shiftNo)
+                    if (ele.shiftNo === mechanicShift) {
+                        shiftDetails = ele;
+                    }
+                    return shiftDetails;
+                }
+            });
+            mechanicLists = await MechanicDetails.MechanicDetails(req, res);
+            subAreas = await SubAreasDetails.subAreas(req, res);
+            breakList = await ShiftBreakTime.ShiftBreakTime(req, res);
+
+            if (shiftDetails) {
+                res.send(mechanicShiftRecords = {
+                    "records": {
+                        "subareas": subAreas,
+                        "shiftStart": shiftDetails.shiftStartTime,
+                        "shiftEnd": shiftDetails.shiftEndTime,
+                        "mechanicList": mechanicLists,
+                        "breakList": breakList
+
+                    }
+                })
             }
-        });
-
-        mechanicLists = await MechanicDetails.MechanicDetails(req, res);
-        subAreas = await SubAreasDetails.subAreas(req, res);
-        breakList = await ShiftBreakTime.ShiftBreakTime(req, res);
-
-        let mechanicShiftRecords = {
-            "records": {
-                "subareas": subAreas,
-                "shiftStart": shiftDetails.shiftStartTime,
-                "shiftEnd": shiftDetails.shiftEndTime,
-                "mechanicList": mechanicLists,
-                "breakList": breakList
-
-            }
-        }
-        if (mechanicShiftRecords) {
-            res.send(mechanicShiftRecords);
+            else { res.send(`${userId} does not have any shift in current time zone`) }
         }
         else {
-            res.send("Shift Records not found")
+            res.send(`Shift Records not found for this ${userId}!Please verify the SNumber`)
         }
-
-
-
     }
-
     catch (err) {
         console.log("Error - ", err);
         res.status(500).render('error', { error: err })
