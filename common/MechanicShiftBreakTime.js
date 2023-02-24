eb287d158e5e70c55077a61ca7c58eb7b21c1955
nandini-ravi <nanravi@deloitte.com>
2023-02-24 20:24:59 +0530

    Updated the breaklist

--- common/MechanicShiftBreakTime.js
+++ common/MechanicShiftBreakTime.js
@@ -7,24 +7,25 @@ const MechanicShiftBreakTime = async (req, res) => {
     //to get the day number 
     daynumber = new Date().getDay()
 
-    const breakDetails = SHIFT_BREAK_TIMINGS[daynumber].map(ele => {
-                breakList.push({
-                    "breakName": ele.breakDescription,
-                    "breakDescription": ele.breakDescription,
-                    "breakStartTime": ele.breakStartTime,
-                    "breakEndTime": ele.breakEndTime,
-                    "shiftNo": ele.shiftNo
-                },{
-                    "breakName": ele.mealDescription,
-                    "breakDescription": ele.mealDescription,
-                    "breakStartTime": ele.mealStartTime,
-                    "breakEndTime": ele.mealEndTime,
-                    "shiftNo": ele.shiftNo
-                });
-                return breakList;
-            });
-    
-    return breakDetails;
+    const dayDetails = SHIFT_BREAK_TIMINGS[daynumber]
+    dayDetails.forEach(ele => {
+        breakList.push({
+            "breakName": ele.breakDescription,
+            "breakDescription": ele.breakDescription,
+            "breakStartTime": ele.breakStartTime,
+            "breakEndTime": ele.breakEndTime,
+            "shiftNo": ele.shiftNo
+        },{
+            "breakName": ele.mealDescription,
+            "breakDescription": ele.mealDescription,
+            "breakStartTime": ele.mealStartTime,
+            "breakEndTime": ele.mealEndTime,
+            "shiftNo": ele.shiftNo
+        })
+    });
+
+    //console.log("breakList: ",breakList);
+    return breakList;
 }
 
 module.exports = {
