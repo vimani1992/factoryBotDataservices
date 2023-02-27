ef0a884eda853b5072e3e126b1d9e572969b110d
nandini-ravi <nanravi@deloitte.com>
2023-02-27 03:04:04 -0500

    Updated the timings to epoch format

--- common/MechanicShiftBreakTime.js
+++ common/MechanicShiftBreakTime.js
@@ -5,21 +5,30 @@ const MechanicShiftBreakTime = async (req, res) => {
     let breakList = [];
 
     //to get the day number 
-    daynumber = new Date().getDay()
+    daynumber = new Date().getDay();
+
+    let d = new Date();
+
+    let currentYear = d.getUTCFullYear();
+    let currentMonth = d.getUTCMonth()
+    let currentDate = d.getUTCDate();
 
     const dayDetails = SHIFT_BREAK_TIMINGS[daynumber]
     dayDetails.forEach(ele => {
+        if(ele.shiftNo === 3){
+            currentDate = currentDate + 1
+        }
         breakList.push({
             "breakName": ele.breakDescription,
             "breakDescription": ele.breakDescription,
-            "breakStartTime": ele.breakStartTime,
-            "breakEndTime": ele.breakEndTime,
+            "breakStartTime": new Date(currentYear, currentMonth, currentDate, ele.breakStartTime.split(':')[0],ele.breakStartTime.split(':')[1],ele.breakStartTime.split(':')[2]).getTime(),
+            "breakEndTime": new Date(currentYear, currentMonth, currentDate, ele.breakEndTime.split(':')[0],ele.breakEndTime.split(':')[1],ele.breakEndTime.split(':')[2]).getTime(),
             "shiftNo": ele.shiftNo
         },{
             "breakName": ele.mealDescription,
             "breakDescription": ele.mealDescription,
-            "breakStartTime": ele.mealStartTime,
-            "breakEndTime": ele.mealEndTime,
+            "breakStartTime": new Date(currentYear, currentMonth, currentDate, ele.mealStartTime.split(':')[0],ele.mealStartTime.split(':')[1],ele.mealStartTime.split(':')[2]).getTime(),
+            "breakEndTime": new Date(currentYear, currentMonth, currentDate, ele.mealEndTime.split(':')[0],ele.mealEndTime.split(':')[1],ele.mealEndTime.split(':')[2]).getTime(),
             "shiftNo": ele.shiftNo
         })
     });
