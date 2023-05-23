17a588bda2fbe16b1677caeb8465935e26637173
Sharma <vrisharma@deloitte.com>
2023-05-23 13:13:00 +0530

    added areas and subareas

--- constants/area_subarea.js
+++ constants/area_subarea.js
@@ -1,42 +1,108 @@
 module.exports  = {
         "A001" : { 
             "areaId" : "A001",
-            "areaName" : "Area Name 001",
+            "areaName" : "11 Wingbox",
             "subarea" : [
-                "SubArea0011",
-                "SubArea0012",
-                "SubArea0013",
-                "SubArea0014"
+                "ACC 415"
             ]
         },
         "A002" : { 
             "areaId" : "A002",
-            "areaName" : "Area Name 002",
+            "areaName" : "43/46 Lowers",
             "subarea" : [
-                "SubArea0021",
-                "SubArea0022",
-                "SubArea0023",
-                "SubArea0024"
+                "Floor Grid",
+                "Position 4A",
+                "Position 5"
             ]
         },
         "A003" : { 
             "areaId" : "A003",
-            "areaName" : "Area Name 003",
+            "areaName" : "43/46 Pre Integrations",
             "subarea" : [
-                "SubArea0031",
-                "SubArea0032",
-                "SubArea0033",
-                "SubArea0034"
+                "43 Pre Integrations Lowers",
+                "46 Pre Integrations Lowers",
+                "Hogshed"
             ]
         },
         "A004" : { 
             "areaId" : "A004",
-            "areaName" : "Area Name 004",
+            "areaName" : "44/Berthas",
             "subarea" : [
-                "SubArea0041",
-                "SubArea0042",
-                "SubArea0043",
-                "SubArea0044"
+                "ACC 447/448"
+            ]
+        },
+        "A005" : { 
+            "areaId" : "A005",
+            "areaName" : "44 Join",
+            "subarea" : [
+                "ACC 480",
+                "ACC 489",
+                "ACC 490"
+            ]
+        }
+        ,"A006" : { 
+            "areaId" : "A006",
+            "areaName" : "44 Balcony",
+            "subarea" : [
+                "Backshop",
+                "Frontshop"
+            ]
+        },
+        "A007" : { 
+            "areaId" : "A007",
+            "areaName" : "47/48 Join",
+            "subarea" : [
+                "ACC 782",
+                "ACCC 785"
+            ]
+        },
+        "A008" : { 
+            "areaId" : "A008",
+            "areaName" : "737 Section 41 Feeders NEMF",
+            "subarea" : [
+                "Cab Build",
+                "Entry Doors",
+                "Galley Doors",
+                "Nose Wheel Well"
+            ]
+        },
+        "A009" : { 
+            "areaId" : "A009",
+            "areaName" : "737 Section 41 Join NEMF",
+            "subarea" : [
+                "ACC 185",
+                "ACC 190"
+            ]
+        },
+        "A010" : { 
+            "areaId" : "A010",
+            "areaName" : "Bonding",
+            "subarea" : [
+                "Autoclave/Breakout",
+                "Bond Room",
+                "Lam Room",
+                "Wheat Starch"
+            ]
+        },
+        "A011" : { 
+            "areaId" : "A011",
+            "areaName" : "Integration",
+            "subarea" : [
+                "AFDE-Seat Tracks",
+                "Doors",
+                "Lower",
+                "Upper"
+            ]
+        },
+        "A012" : { 
+            "areaId" : "A012",
+            "areaName" : "Tulsa 737 Section 11",
+            "subarea" : [
+                "Forward Spar",
+                "Pressure Floor",
+                "Sluggers & Drivmatic",
+                "Spanvise Beams",
+                "Upper"
             ]
         }
 };
